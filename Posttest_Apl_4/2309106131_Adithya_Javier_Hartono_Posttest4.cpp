d4599795ef77d047812e9b9c6e34d9e69fac9f3e
javierhart <161821933+javierhart@users.noreply.github.com>
2024-03-24 12:58:35 +0700

    Add files via upload

---
+++ Posttest_Apl_4/2309106131_Adithya_Javier_Hartono_Posttest4.cpp
@@ -0,0 +1,285 @@
+
+
+#include <iostream>
+#include <string>
+#include <limits>
+#include <iomanip>
+#include <algorithm>
+#include <cstdlib>
+
+using namespace std;
+
+const int max_hero = 100;
+
+struct Ability {
+    string name;
+    int damage;
+    int difficulty; 
+};
+
+struct Hero {
+    string nama;
+    string role;
+    Ability abilities[1];
+};
+
+Hero hero[max_hero];
+int heroCount = 0;
+
+bool nama_duplikat(const string& nama_baru, const string& role);
+void clearScreen();
+void tambah();
+void edit();
+void lihat_hero();
+void hapus();
+bool login();
+int hitung_role_hero(const string& role, int index = 0, int count = 0); // Fungsi rekursif
+
+int main() {
+    if (!login()) {
+        return 0;
+    }
+
+    int pil;
+    do {
+        system("cls");
+        cout << "Honor of King`s:\n";
+        cout << "1. Tambah Hero Meta\n";
+        cout << "2. Lihat Hero Meta\n";
+        cout << "3. Hapus Hero Meta\n";
+        cout << "4. Ubah Hero Meta\n";
+        cout << "5. Hitung Jumlah Hero di dalam role\n";
+        cout << "0. Keluar\n";
+        cout << "Pilih: ";
+        cin >> pil;
+
+        switch (pil) {
+            case 1:
+                tambah();
+                break;
+            case 2:
+                lihat_hero();
+                break;
+            case 3:
+                hapus();
+                break;
+            case 4:
+                edit();
+                break;
+            case 5:
+                {
+                    clearScreen();
+                    cout << "Hitung Jumlah Hero dalam role\n";
+                    cout << "Pilih Role (Assassin, Mid, Farm, Clash, Support, Tank): ";
+                    string role;
+                    cin >> role;
+                    int count = hitung_role_hero(role);
+                    cout << "Jumlah hero dalam role " << role << " adalah: " << count << endl;
+                    cout << "Tekan Enter untuk melanjutkan...";
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    cin.get();
+                }
+                break;
+            case 0:
+                cout << "Ihhh jahat banget mau pergi sekarang-_-  Yaudah Selamat Tinggal >///<\n";
+                return 0; 
+            default:
+                cout << "Pilihan tidak valid.\n";
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    } while (pil != 0); 
+
+    return 0;
+}
+
+
+bool nama_duplikat(const string& nama_baru, const string& role) {
+    for (int i = 0; i < heroCount; ++i) {
+        if (hero[i].nama == nama_baru && hero[i].role == role) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void clearScreen() {
+    system("cls");
+}
+void tambah() {
+    clearScreen();
+    cout << "Tambah Hero Meta:\n";
+    cout << "Role Tersedia (Assassin, Mid, Farm, Clash, Support, Tank)\n";
+
+    cout << "Nama: ";
+    string nama_baru;
+    getline(cin >> ws, nama_baru);
+
+    cout << "Role: ";
+    string role_baru;
+    getline(cin >> ws, role_baru);
+
+    Ability ability;
+
+    cout << "Tingkat Damage (1-5): ";
+    cin >> ability.damage;
+    cout << "Kesulitan Hero(1-5): ";
+    cin >> ability.difficulty;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    hero[heroCount].nama = nama_baru;
+    hero[heroCount].role = role_baru;
+    hero[heroCount].abilities[0] = ability;
+    heroCount++;
+
+    cout << "Berhasil ditambahkan.\n";
+    cout << "Tekan Enter untuk kembali ke menu utama.\n";
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+    cin.get();
+}
+
+
+void edit() {
+    clearScreen();
+    cout << "Ubah Hero Meta\n";
+    cout << "Pilih Role (Assassin, Mid, Farm, Clash, Support, Tank): ";
+    string role;
+    getline(cin >> ws, role);
+
+    bool found = false;
+    for (int i = 0; i < heroCount; ++i) {
+        if (hero[i].role == role) {
+            found = true;
+            cout << i + 1 << ". " << hero[i].nama << endl;
+        }
+    }
+
+    if (!found) {
+        cout << "Hero yang anda cari tidak ada dalam role tersebut!\n";
+        return;
+    }
+
+    cout << "Pilih nomor hero yang ingin diganti: ";
+    int pil;
+    cin >> pil;
+    if (pil > 0 && pil <= heroCount) {
+        
+        cout << "Masukkan nama hero: ";
+        string nama_baru;
+        getline(cin >> ws, nama_baru);
+
+        if (nama_baru != hero[pil - 1].nama && nama_duplikat(nama_baru, role)) {
+            cout << "Hero sudah ada dalam role tersebut!\n";
+            return;
+        }
+
+        Ability ability;
+
+        cout << "Tingkat Damage (1-5): ";
+        cin >> ability.damage;
+        cout << "Kesulitan Hero(1-5): ";
+        cin >> ability.difficulty;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        hero[pil - 1].nama = nama_baru;
+        cout << "Hero berhasil diubah >///<\n";
+    } else {
+        cout << "Input tidak valid!\n";
+    }
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cin.get();
+}
+
+void lihat_hero() {
+    clearScreen();
+    cout << "List Hero Meta \n";
+    cout << setw(20) << left << "Nama" << setw(20) << "Role" << setw(20) << "Abilities" << endl;
+    cout << "--------------------------------------------------------------------\n";
+    for (int i = 0; i < heroCount; ++i) {
+        cout << setw(20) << left << hero[i].nama << setw(20) << hero[i].role;
+        cout << setw(15) << left << "Damage: " << hero[i].abilities[0].damage;
+        cout << setw(10) << left << "Kesulitan: " << hero[i].abilities[0].difficulty;
+        cout << endl;
+    }
+    cout << "Tekan Enter untuk kembali ke menu utama.\n";
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+    cin.get();
+}
+
+
+
+void hapus() {
+    clearScreen();
+    cout << "Hapus Hero Meta\n";
+    cout << "Pilih Role (Assassin, Mid, Farm, Clash, Support, Tank): ";
+    string role;
+    getline(cin >> ws, role);
+
+    bool found = false;
+    for (int i = 0; i < heroCount; ++i) {
+        if (hero[i].role == role) {
+            found = true;
+            cout << i + 1 << ". " << hero[i].nama << endl;
+        }
+    }
+
+    if (!found) {
+        cout << "Hero yang anda cari tidak ada dalam role tersebut!\n";
+        return;
+    }
+
+    cout << "Pilih nomor hero yang ingin dihapus: ";
+    int pil;
+    cin >> pil;
+    if (pil > 0 && pil <= heroCount) {
+        for (int i = pil - 1; i < heroCount - 1; ++i) {
+            hero[i] = hero[i + 1];
+        }
+        heroCount--;
+        cout << "Hero berhasil dihapus\n";
+    } else {
+        cout << "Input tidak valid!\n";
+    }
+
+    cout << "Tekan Enter untuk kembali ke menu utama.\n";
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cin.get();
+}
+
+bool login() {
+    int attempts = 3;
+    string username, password;
+    clearScreen();
+    while (attempts > 0) {
+        cout << "Log in Honor of King`s\n\n";
+        cout << "Username: ";
+        getline(cin, username);
+        cout << "Kata Sandi: ";
+        getline(cin, password);
+
+        if (username == "javier" && password == "131") {
+            cout << "Selamat datang, " << username << "!" << endl;
+            return true;
+        } else {
+            attempts--;
+            cout << "Username atau password salah!\n";
+            if (attempts == 0) {
+                cout << "AKUN TERBLOKIR!!!" << endl;
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                return false;
+            }
+        }
+    }
+    return false;
+}
+
+
+int hitung_role_hero(const string& role, int index, int count) {
+    if (index >= heroCount) {
+        return count;
+    }
+    if (hero[index].role == role) {
+        count++;
+    }
+    return hitung_role_hero(role, index + 1, count);
+}
